fb09cc2e65156b203353699b6539d21b35b20361
Kdy010010 <137839917+Kdy010010@users.noreply.github.com>
2023-12-15 13:53:55 +0900

    Update Movey.js

--- Movey.js
+++ Movey.js
@@ -1,4 +1,4 @@
-class MoveyElement extends HTMLElement {
+  class MoveyElement extends HTMLElement {
     constructor() {
       super();
 
@@ -11,36 +11,43 @@ class MoveyElement extends HTMLElement {
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
-      if (oldValue !== newValue && name === 'move') {
+      if ((oldValue !== newValue) && (name === 'move' || name === 'objectid')) {
         this.setupMovement();
       }
     }
 
     static get observedAttributes() {
-      return ['move'];
+      return ['move', 'objectid'];
     }
 
     setupMovement() {
       const moveValue = parseInt(this.getAttribute('move')) || 0;
+      const objectId = this.getAttribute('objectid');
 
-      this.shadowRoot.innerHTML = `
-        <style>
-          :host {
-            display: inline-block;
-            position: relative;
-          }
-        </style>
-        <slot></slot>
-        <script>
-          document.addEventListener('DOMContentLoaded', function() {
-            const slotContent = document.querySelector('movey-element').shadowRoot.querySelector('slot').assignedNodes()[0];
-            const element = slotContent.querySelector('div');
-            
-            element.style.transition = 'transform 0.5s ease-in-out';
-            element.style.transform = 'translateY(${moveValue}px)';
-          });
-        </script>
-      `;
+      const element = document.getElementById(objectId);
+
+      if (element) {
+        this.shadowRoot.innerHTML = `
+          <style>
+            :host {
+              display: inline-block;
+              position: relative;
+            }
+          </style>
+          <slot></slot>
+          <script>
+            document.addEventListener('DOMContentLoaded', function() {
+              const slotContent = document.querySelector('movey-element').shadowRoot.querySelector('slot').assignedNodes()[0];
+              
+              const element = document.getElementById('${objectId}');
+              element.style.transition = 'transform 0.5s ease-in-out';
+              element.style.transform = 'translateY(${moveValue}px)';
+            });
+          </script>
+        `;
+      } else {
+        console.error(`Element with id '${objectId}' not found.`);
+      }
     }
 
     render() {
